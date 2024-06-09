ae3a6cdb435b45495589ef4ee3d93a41b033f161
CasianTheProgrammator <99408730+casian2018@users.noreply.github.com>
2024-06-09 11:17:07 +0300

    update

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -26,7 +26,7 @@ export default function Home() {
               </a>
               <a
                 className="inline-block rounded-lg px-2 py-1 text-sm font-medium text-gray-900 transition-all duration-200 hover:bg-gray-100 hover:text-gray-900"
-                href="#"
+                href="/pricing"
               >
                 Pricing
               </a>
